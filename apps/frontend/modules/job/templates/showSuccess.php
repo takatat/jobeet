4ae22224c1b9c0cc5569ab64be643e488485790f
Tomonori Takata <takata.to@gmail.com>
2009-10-24 23:03:11 +0900

    See it in Action in the Browser

---
+++ apps/frontend/modules/job/templates/showSuccess.php
@@ -0,0 +1,78 @@
+<table>
+  <tbody>
+    <tr>
+      <th>Id:</th>
+      <td><?php echo $jobeet_job->getid() ?></td>
+    </tr>
+    <tr>
+      <th>Category:</th>
+      <td><?php echo $jobeet_job->getcategory_id() ?></td>
+    </tr>
+    <tr>
+      <th>Type:</th>
+      <td><?php echo $jobeet_job->gettype() ?></td>
+    </tr>
+    <tr>
+      <th>Company:</th>
+      <td><?php echo $jobeet_job->getcompany() ?></td>
+    </tr>
+    <tr>
+      <th>Logo:</th>
+      <td><?php echo $jobeet_job->getlogo() ?></td>
+    </tr>
+    <tr>
+      <th>Url:</th>
+      <td><?php echo $jobeet_job->geturl() ?></td>
+    </tr>
+    <tr>
+      <th>Position:</th>
+      <td><?php echo $jobeet_job->getposition() ?></td>
+    </tr>
+    <tr>
+      <th>Location:</th>
+      <td><?php echo $jobeet_job->getlocation() ?></td>
+    </tr>
+    <tr>
+      <th>Description:</th>
+      <td><?php echo $jobeet_job->getdescription() ?></td>
+    </tr>
+    <tr>
+      <th>How to apply:</th>
+      <td><?php echo $jobeet_job->gethow_to_apply() ?></td>
+    </tr>
+    <tr>
+      <th>Token:</th>
+      <td><?php echo $jobeet_job->gettoken() ?></td>
+    </tr>
+    <tr>
+      <th>Is public:</th>
+      <td><?php echo $jobeet_job->getis_public() ?></td>
+    </tr>
+    <tr>
+      <th>Is activated:</th>
+      <td><?php echo $jobeet_job->getis_activated() ?></td>
+    </tr>
+    <tr>
+      <th>Email:</th>
+      <td><?php echo $jobeet_job->getemail() ?></td>
+    </tr>
+    <tr>
+      <th>Expires at:</th>
+      <td><?php echo $jobeet_job->getexpires_at() ?></td>
+    </tr>
+    <tr>
+      <th>Created at:</th>
+      <td><?php echo $jobeet_job->getcreated_at() ?></td>
+    </tr>
+    <tr>
+      <th>Updated at:</th>
+      <td><?php echo $jobeet_job->getupdated_at() ?></td>
+    </tr>
+  </tbody>
+</table>
+
+<hr />
+
+<a href="<?php echo url_for('job/edit?id='.$jobeet_job->getId()) ?>">Edit</a>
+&nbsp;
+<a href="<?php echo url_for('job/index') ?>">List</a>
---
+++ lib/form/doctrine/base/BaseJobeetCategoryForm.class.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * JobeetCategory form base class.
+ *
+ * @package    form
+ * @subpackage jobeet_category
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 8508 2008-04-17 17:39:15Z fabien $
+ */
+class BaseJobeetCategoryForm extends BaseFormDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'id'                     => new sfWidgetFormInputHidden(),
+      'name'                   => new sfWidgetFormInput(),
+      'created_at'             => new sfWidgetFormDateTime(),
+      'updated_at'             => new sfWidgetFormDateTime(),
+      'jobeet_affiliates_list' => new sfWidgetFormDoctrineChoiceMany(array('model' => 'JobeetAffiliate')),
+    ));
+
+    $this->setValidators(array(
+      'id'                     => new sfValidatorDoctrineChoice(array('model' => 'JobeetCategory', 'column' => 'id', 'required' => false)),
+      'name'                   => new sfValidatorString(array('max_length' => 255)),
+      'created_at'             => new sfValidatorDateTime(array('required' => false)),
+      'updated_at'             => new sfValidatorDateTime(array('required' => false)),
+      'jobeet_affiliates_list' => new sfValidatorDoctrineChoiceMany(array('model' => 'JobeetAffiliate', 'required' => false)),
+    ));
+
+    $this->validatorSchema->setPostValidator(
+      new sfValidatorDoctrineUnique(array('model' => 'JobeetCategory', 'column' => array('name')))
+    );
+
+    $this->widgetSchema->setNameFormat('jobeet_category[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'JobeetCategory';
+  }
+
+  public function updateDefaultsFromObject()
+  {
+    parent::updateDefaultsFromObject();
+
+    if (isset($this->widgetSchema['jobeet_affiliates_list']))
+    {
+      $this->setDefault('jobeet_affiliates_list', $this->object->JobeetAffiliates->getPrimaryKeys());
+    }
+
+  }
+
+  protected function doSave($con = null)
+  {
+    parent::doSave($con);
+
+    $this->saveJobeetAffiliatesList($con);
+  }
+
+  public function saveJobeetAffiliatesList($con = null)
+  {
+    if (!$this->isValid())
+    {
+      throw $this->getErrorSchema();
+    }
+
+    if (!isset($this->widgetSchema['jobeet_affiliates_list']))
+    {
+      // somebody has unset this widget
+      return;
+    }
+
+    if (is_null($con))
+    {
+      $con = $this->getConnection();
+    }
+
+    $existing = $this->object->JobeetAffiliates->getPrimaryKeys();
+    $values = $this->getValue('jobeet_affiliates_list');
+    if (!is_array($values))
+    {
+      $values = array();
+    }
+
+    $unlink = array_diff($existing, $values);
+    if (count($unlink))
+    {
+      $this->object->unlink('JobeetAffiliates', array_values($unlink));
+    }
+
+    $link = array_diff($values, $existing);
+    if (count($link))
+    {
+      $this->object->link('JobeetAffiliates', array_values($link));
+    }
+  }
+
+}
