f64b8b4f48216d98b8cdc45014bc4d8150149875
Tomonori Takata <takata.to@gmail.com>
2009-10-26 00:02:50 +0900

    __toString()

--- lib/model/doctrine/JobeetAffiliate.class.php
+++ lib/model/doctrine/JobeetAffiliate.class.php
@@ -5,5 +5,8 @@
  */
 class JobeetAffiliate extends BaseJobeetAffiliate
 {
-
+  public function __toString()
+  {
+    return $this->getUrl();
+  }
 }
--- lib/model/doctrine/JobeetJob.class.php
+++ lib/model/doctrine/JobeetJob.class.php
@@ -5,5 +5,8 @@
  */
 class JobeetJob extends BaseJobeetJob
 {
-
+  public function __toString()
+  {
+    return sprintf('%s at %s (%s)', $this->getPosition(), $this->getCompany(), $this->getLocation());
+  }
 }
