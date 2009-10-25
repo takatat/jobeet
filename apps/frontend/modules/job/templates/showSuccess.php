4cd7f3a1d006d799ba3e95eae5f2d427eddac63b
Tomonori Takata <takata.to@gmail.com>
2009-10-26 00:27:22 +0900

    The Template

--- apps/frontend/modules/job/templates/showSuccess.php
+++ apps/frontend/modules/job/templates/showSuccess.php
@@ -1,79 +1,38 @@
 <?php use_stylesheet('job.css') ?>
-<table>
-  <tbody>
-    <tr>
-      <th>Id:</th>
-      <td><?php echo $jobeet_job->getid() ?></td>
-    </tr>
-    <tr>
-      <th>Category:</th>
-      <td><?php echo $jobeet_job->getcategory_id() ?></td>
-    </tr>
-    <tr>
-      <th>Type:</th>
-      <td><?php echo $jobeet_job->gettype() ?></td>
-    </tr>
-    <tr>
-      <th>Company:</th>
-      <td><?php echo $jobeet_job->getcompany() ?></td>
-    </tr>
-    <tr>
-      <th>Logo:</th>
-      <td><?php echo $jobeet_job->getlogo() ?></td>
-    </tr>
-    <tr>
-      <th>Url:</th>
-      <td><?php echo $jobeet_job->geturl() ?></td>
-    </tr>
-    <tr>
-      <th>Position:</th>
-      <td><?php echo $jobeet_job->getposition() ?></td>
-    </tr>
-    <tr>
-      <th>Location:</th>
-      <td><?php echo $jobeet_job->getlocation() ?></td>
-    </tr>
-    <tr>
-      <th>Description:</th>
-      <td><?php echo $jobeet_job->getdescription() ?></td>
-    </tr>
-    <tr>
-      <th>How to apply:</th>
-      <td><?php echo $jobeet_job->gethow_to_apply() ?></td>
-    </tr>
-    <tr>
-      <th>Token:</th>
-      <td><?php echo $jobeet_job->gettoken() ?></td>
-    </tr>
-    <tr>
-      <th>Is public:</th>
-      <td><?php echo $jobeet_job->getis_public() ?></td>
-    </tr>
-    <tr>
-      <th>Is activated:</th>
-      <td><?php echo $jobeet_job->getis_activated() ?></td>
-    </tr>
-    <tr>
-      <th>Email:</th>
-      <td><?php echo $jobeet_job->getemail() ?></td>
-    </tr>
-    <tr>
-      <th>Expires at:</th>
-      <td><?php echo $jobeet_job->getexpires_at() ?></td>
-    </tr>
-    <tr>
-      <th>Created at:</th>
-      <td><?php echo $jobeet_job->getcreated_at() ?></td>
-    </tr>
-    <tr>
-      <th>Updated at:</th>
-      <td><?php echo $jobeet_job->getupdated_at() ?></td>
-    </tr>
-  </tbody>
-</table>
-
-<hr />
-
-<a href="<?php echo url_for('job/edit?id='.$jobeet_job->getId()) ?>">Edit</a>
-&nbsp;
-<a href="<?php echo url_for('job/index') ?>">List</a>
+<?php use_helper('Text') ?>
+ 
+<div id="job">
+  <h1><?php echo $job->getCompany() ?></h1>
+  <h2><?php echo $job->getLocation() ?></h2>
+  <h3>
+    <?php echo $job->getPosition() ?>
+    <small> - <?php echo $job->getType() ?></small>
+  </h3>
+ 
+  <?php if ($job->getLogo()): ?>
+    <div class="logo">
+      <a href="<?php echo $job->getUrl() ?>">
+        <img src="/uploads/jobs/<?php echo $job->getLogo() ?>"
+          alt="<?php echo $job->getCompany() ?> logo" />
+      </a>
+    </div>
+  <?php endif; ?>
+ 
+  <div class="description">
+    <?php echo simple_format_text($job->getDescription()) ?>
+  </div>
+ 
+  <h4>How to apply?</h4>
+ 
+  <p class="how_to_apply"><?php echo $job->getHowToApply() ?></p>
+ 
+  <div class="meta">
+    <small>posted on <?php echo date('m/d/Y', strtotime($job->getCreatedAt())) ?></small>
+  </div>
+ 
+  <div style="padding: 20px 0">
+    <a href="<?php echo url_for('job/edit?id='.$job->getId()) ?>">
+      Edit
+    </a>
+  </div>
+</div>
