71e13b25cbc3822daab1fadf2453132374e7c90e
Tomonori Takata <takata.to@gmail.com>
2009-11-10 01:06:22 +0900

    Backend Security

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -18,6 +18,7 @@
         </h1>
       </div>
  
+    <?php if ($sf_user->isAuthenticated()): ?>
       <div id="menu">
         <ul>
           <li>
@@ -26,9 +27,12 @@
           <li>
             <?php echo link_to('Categories', '@jobeet_category_category') ?>
           </li>
+          <li><?php echo link_to('Users', '@sf_guard_user') ?></li>
+          <li><?php echo link_to('Logout', '@sf_guard_signout') ?></li>
         </ul>
       </div>
- 
+    <?php endif; ?>
+
       <div id="content">
         <?php echo $sf_content ?>
       </div>
