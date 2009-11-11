63cc967534fed6957abb59ad2de967e9086c0c34
Tomonori Takata <takata.to@gmail.com>
2009-11-12 00:31:33 +0900

    The Job Web Service

--- lib/model/doctrine/JobeetJob.class.php
+++ lib/model/doctrine/JobeetJob.class.php
@@ -81,4 +81,20 @@ class JobeetJob extends BaseJobeetJob
   {
     return floor((strtotime($this->getExpiresAt()) - time()) / 86400);
   }
+
+  public function asArray($host)
+  {
+    return array(
+      'category'     => $this->getJobeetCategory()->getName(),
+      'type'         => $this->getType(),
+      'company'      => $this->getCompany(),
+      'logo'         => $this->getLogo() ? 'http://'.$host.'/uploads/jobs/'.$this->getLogo() : null,
+      'url'          => $this->getUrl(),
+      'position'     => $this->getPosition(),
+      'location'     => $this->getLocation(),
+      'description'  => $this->getDescription(),
+      'how_to_apply' => $this->getHowToApply(),
+      'expires_at'   => $this->getCreatedAt(),
+    );
+  }
 }
