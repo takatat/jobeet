8743af6b6ae2e06be67dd67cea8b6bd7afc75e51
Tomonori Takata <takata.to@gmail.com>
2009-11-13 11:58:48 +0900

    Admin Generator

--- lib/form/doctrine/JobeetCategoryForm.class.php
+++ lib/form/doctrine/JobeetCategoryForm.class.php
@@ -12,5 +12,9 @@ class JobeetCategoryForm extends BaseJobeetCategoryForm
   public function configure()
   {
     unset($this['created_at'], $this['updated_at'], $this['jobeet_affiliates_list']);
+ 
+    $this->embedI18n(array('en', 'fr'));
+    $this->widgetSchema->setLabel('en', 'English');
+    $this->widgetSchema->setLabel('fr', 'French');
   }
 }
