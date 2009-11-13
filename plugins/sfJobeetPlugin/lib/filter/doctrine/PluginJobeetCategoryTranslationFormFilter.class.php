4623a885aaa05e19e7a32603c8872abe057a53b6
Tomonori Takata <takata.to@gmail.com>
2009-11-13 15:45:42 +0900

    The Model(add)

---
+++ plugins/sfJobeetPlugin/lib/filter/doctrine/PluginJobeetCategoryTranslationFormFilter.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * JobeetCategoryTranslation filter form.
+ *
+ * @package    filters
+ * @subpackage JobeetCategoryTranslation *
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 11675 2008-09-19 15:21:38Z fabien $
+ */
+abstract class PluginJobeetCategoryTranslationFormFilter extends BaseJobeetCategoryTranslationFormFilter
+{
+  public function configure()
+  {
+  }
+}
